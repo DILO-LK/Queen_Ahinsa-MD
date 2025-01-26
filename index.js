874fe04f34621436d2b9af4922792fe45afaa3a5
DILO-LK <dgimshan6@gmail.com>
2025-01-26 09:45:05 +0530

    D    O    N    E    ✅️

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94788240417']
+const ownerNumber = ['94771098429']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
